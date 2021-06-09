a8e4d974e7ae81232de233aefe1796310f5a330d
mausenkova <85360611+mausenkova@users.noreply.github.com>
2021-06-09 15:35:52 +0300

    save as png

---
+++ src/ChipSeeker.R
@@ -0,0 +1,41 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)   # replace_na
+library(tibble)  # column_to_rownames
+###
+
+ if (!requireNamespace("BiocManager", quietly = TRUE))
+   install.packages("BiocManager")
+   BiocManager::install(version = "3.13")
+ BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene")
+ BiocManager::install("clusterProfiler")
+
+library(ChIPseeker)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+#library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+library(clusterProfiler)
+
+###
+
+NAME <- 'H3K9me3_H1.ENCFF697NMG.hg19.filtered'
+#NAME <- 'H3K9me3_H1.ENCFF587TWB.hg19.filtered'
+OUT_DIR<-'C:/Users/Mary/Desktop/дз/Майнор/файлы с сервера/Проект/Results/'
+DATA_DIR<- 'C:/Users/Mary/Desktop/дз/Майнор/файлы с сервера/Проект/'
+BED_FN <- paste0(DATA_DIR, NAME, '.bed')
+
+###
+
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+
+peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Hs.eg.db")
+
+#pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.pdf'))
+png(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.png'))
+plotAnnoPie(peakAnno)
+dev.off()
+
+# peak <- readPeakFile(BED_FN)
+# pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.covplot.pdf'))
+# covplot(peak, weightCol="V5")
+# dev.off()
+# 
